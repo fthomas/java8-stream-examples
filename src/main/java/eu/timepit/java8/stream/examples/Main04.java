e076ef9812afdbf32ca46822ecdcf9c44c601dcc
Frank S. Thomas <frank@timepit.eu>
2015-03-19 09:54:12 +0100

    Parallele Streams

--- src/main/java/eu/timepit/java8/stream/examples/Main04.java
+++ src/main/java/eu/timepit/java8/stream/examples/Main04.java
@@ -4,17 +4,17 @@ import java.util.OptionalInt;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
-public class Main03 {
+public class Main04 {
     public static void main(String[] args) {
         IntPredicate isEven = i -> {
             System.out.println(i);
             return i % 2 == 0;
         };
 
-        final OptionalInt first = IntStream.range(1, 10).filter(isEven).findFirst();
+        final OptionalInt first = IntStream.range(1, 10).parallel().filter(isEven).findFirst();
         System.out.println("evens.findFirst(): " + first);
 
-        final int sum = IntStream.range(1, 10).filter(isEven).sum();
+        final int sum = IntStream.range(1, 10).parallel().filter(isEven).sum();
         System.out.println("evens.sum(): " + sum);
     }
 }
---
+++ src/main/java/eu/timepit/java8/stream/examples/Main05.java
@@ -0,0 +1,4 @@
+package eu.timepit.java8.stream.examples;
+
+public class Main05 {
+}
